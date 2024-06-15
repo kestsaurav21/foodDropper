2034441c006a544705757e9c3a5ac06dec93069d
kestsaurav21 <kestsaurav21@outlook.com>
2024-06-16 01:52:29 +0530

    feat: fetch api fixed

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,12 +1,41 @@
 import { resList } from "../../utils/apidata";
 import RestaurantCard from "../RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
 
   //State variable -> Super Powerful Variable 
 
   const [ listOfRestaurant, setListOfRestaurant ] = useState(resList);
+
+  // const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+ 
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=28.4594965&lng=77.0266383&carousel=true&third_party_vendor=1'
+);
+
+    const json = await data.json();
+
+    // console.log(json);
+
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+
+
+    // setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    // setFilteredRestaurant(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+
+  
+  };
+
+
+
 
    
     return(
